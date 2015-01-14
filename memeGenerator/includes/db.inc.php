d9da046dc5f702bff87ff0460d864c13c5748563
unknown <rath0064@WT232-15.ottawa.ad.algonquincollege.com>
2015-01-14 17:05:30 -0500

    Meme Genearator Site

--- memeGenerator/includes/db.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbname = "rath0064";
-$dbuser = "rath0064";
-$dbpass = "40776921";
-$dbhost = "localhost";
-$link = new PDO("mysql:host=" . $dbhost . ";dbname=" . $dbname, $dbuser, $dbpass) or die("nothing happening");
-
-?>
